6eb6fdcebb78ee9ad5053422982ac85723706ef0
jakobwilson <jakobwilsonn@gmai.com>
2024-01-17 11:38:33 -0600

    initial commit

--- src/client/services/fetcher.ts
+++ src/client/services/fetcher.ts
@@ -1,34 +1,34 @@
+
 type method_types = "GET" | "POST" | "PUT" | "DELETE";
 
 export const TOKEN_KEY = "token";
 
 interface RequestInfo {
     method: method_types;
-    headers: { [pizza: string]: string };
+    headers: HeadersInit;
     body?: any;
 }
 
 export const fetcher = <T = any>(url: string, method: method_types = "GET", data?: unknown) => {
     return new Promise<T>(async (resolve, reject) => {
         try {
-            const headers = {};
+            const headers: HeadersInit = {};
 
             const fetchOptions: RequestInfo = {
                 method: method,
                 headers,
-                body: JSON.stringify(data),
+                body: data ? JSON.stringify(data) : undefined,
             };
 
             const token = localStorage.getItem(TOKEN_KEY);
+            
 
             if (token) {
-                fetchOptions["headers"]!["Authorization"] = `Bearer ${token}`;
+                headers["Authorization"] = `Bearer ${token}`;
             }
 
-            if (method === "GET" || method === "DELETE") {
-                delete fetchOptions.body;
-            } else {
-                fetchOptions.headers!["Content-Type"] = "application/json";
+            if (method !== "GET" && method !== "DELETE") {
+                headers["Content-Type"] = "application/json";
             }
 
             const SERVER_URL = process.env.SERVER_URL;
